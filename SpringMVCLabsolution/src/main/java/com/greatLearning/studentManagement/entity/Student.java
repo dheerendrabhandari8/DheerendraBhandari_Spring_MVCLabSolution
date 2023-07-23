944af8f794fbbbde74b10a93b0aa4b6146d83540
dheerendra_bhandari <dheerendrabhandari@gmail.com>
2023-07-23 19:30:47 +0530

    Dheerendra assignment

---
+++ SpringMVCLabsolution/src/main/java/com/greatLearning/studentManagement/entity/Student.java
@@ -0,0 +1,96 @@
+package com.greatLearning.studentManagement.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="student")
+public class Student {
+
+	// define fields
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+
+
+	@Column(name="firstName")
+	private String firstName;
+
+
+	@Column(name="lastName")
+	private String lastName;
+
+
+	@Column(name="Department")
+	private String Department;
+	
+	@Column(name="country")
+	private String country;
+
+	public Student()
+	{
+		
+	}
+	
+	public Student(String firstName, String lastName, String Department, String country) {
+		
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.Department = Department;
+		this.country = country;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getDepartment() {
+		return Department;
+	}
+
+	public void setDepartment(String Department) {
+		this.Department = Department;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", Department=" + Department
+				+ ", country=" + country + "]";
+	}
+	
+}
